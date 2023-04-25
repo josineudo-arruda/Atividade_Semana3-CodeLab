3667133ca369b8f12b911cee02c9a327e2f67640
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-25 13:39:02 -0300

    feat: method findById()

--- src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
@@ -6,7 +6,10 @@ import academy.devdojo.springbootessentials.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +28,14 @@ public class AnimeController {
     //public AnimeController(DateUtil dateUtil) {this.dateUtil = dateUtil;}
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         //log.info(dateUtil.formatLocalTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        //log.info(dateUtil.formatLocalTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
+    }
 }
--- src/main/java/academy/devdojo/springbootessentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springbootessentials/service/AnimeService.java
@@ -1,14 +1,24 @@
 package academy.devdojo.springbootessentials.service;
 
 import academy.devdojo.springbootessentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1l, "BNH"), new Anime(2l, "Sailor Moon"));
     //private final AnimeRepository animeRepository;
     public List<Anime> listAll(){
-        return List.of(new Anime(1l, "BNH"), new Anime(2l, "Sailor Moon"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
     }
 }
