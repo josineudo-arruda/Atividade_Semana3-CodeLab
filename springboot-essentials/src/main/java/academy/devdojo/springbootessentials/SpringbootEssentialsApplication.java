752173ebeb2edefd9367de303127babf8725d92d
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-25 13:13:10 -0300

    feat: new project created by spring.initialzr

---
+++ springboot-essentials/src/main/java/academy/devdojo/springbootessentials/SpringbootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springbootessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+}
