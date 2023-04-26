9a2651359af5a4a599b788c54c012c5d8ab7564e
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-26 05:29:01 -0300

    fix: problems in postgre configuration server

--- src/main/java/academy/devdojo/springbootessentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springbootessentials/domain/Anime.java
@@ -1,13 +1,35 @@
 package academy.devdojo.springbootessentials.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
+@Table(name = "anime")
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
 
+    public Anime() {}
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
