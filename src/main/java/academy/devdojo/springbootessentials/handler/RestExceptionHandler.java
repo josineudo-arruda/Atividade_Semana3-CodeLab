f7704d0181dbf3f2e086252f193b481dd219805f
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-29 17:44:04 -0300

    feat: badRequestExceptionDetails

---
+++ src/main/java/academy/devdojo/springbootessentials/handler/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package academy.devdojo.springbootessentials.handler;
+
+import academy.devdojo.springbootessentials.exception.BadRequestException;
+import academy.devdojo.springbootessentials.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(badRequestException.getMessage())
+                        .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+}
